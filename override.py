71d973b1522509e3bd1752f570dc0bd9e2e2240b
Maxime Le Conte des Floris <git@mlcdf.fr>
2021-12-30 12:14:32 +0100

    feat: override file in yaml

--- override.py
+++ override.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 
-from typing import List, Dict
+from typing import List, Dict, Any
 
 import argparse
 import json
 import logging
 
+import yaml
 
-OVERRIDE_FILE = "override.json"
+OVERRIDE_FILE = "override.yaml"
 
 
 def merge_collection(main: List[Dict], override: List[Dict]):
@@ -57,6 +58,51 @@ def clean_genres(collection: List[Dict]):
 
     return collection
 
+def load_override():
+    try:
+        with open(OVERRIDE_FILE, "r") as fd:
+            override = yaml.load(fd, yaml.Loader)
+            if not isinstance(override, list):
+                raise Exception(f"{OVERRIDE_FILE} should contains a list of dict.")
+            return override
+    except (FileNotFoundError):
+        logging.exception("no override file found")
+        return []
+
+def preprocessing_override(override: List[Dict[str, Any]]):
+    for item in override:
+
+        # id: 293857423
+        # -->
+        # id : "293857423"
+        if isinstance(item["id"], int):
+            item["id"] = str(item["id"])
+        
+        # genres: Romance, Drame
+        # -->
+        # genres:
+        #   - Romance
+        #   - Drame
+        if isinstance(item["genres"], str):
+            item["genres"] = [ genre.strip() for genre in item["genres"].split(",") ]
+        
+        # genres:
+        #   - Romance, Drame
+        # -->
+        # genres:
+        #   - Romance
+        #   - Drame
+        if isinstance(item["genres"], list):
+            genres = []
+            for genre in item["genres"]:
+                genres += [ g.strip() for g in genre.split(",") ]
+
+            item["genres"] = genres
+        
+        if "review" in item:
+            item["review"] = item["review"].strip()
+
+    return override
 
 def main():
     parser = argparse.ArgumentParser()
@@ -69,15 +115,7 @@ def main():
     data = json.load(args.file)
     args.file.close()
 
-    try:
-        with open(OVERRIDE_FILE, "r") as fd:
-            override = json.load(fd)
-            if not isinstance(override, list):
-                raise Exception(f"{OVERRIDE_FILE} should contains a list of dict.")
-    except (FileNotFoundError):
-        logging.exception("not override file found")
-        override = []
-
+    override = preprocessing_override(load_override())
     data["entries"] = merge_collection(clean_genres(data["entries"]), override)
 
     if not args.in_place:
